b027e2edc5e8a1f98c3d848fe46f01cb22050479
LeeSeokHo <rrgks@naver.com>
2025-11-03 14:37:48 +0900

    refactor: 현재 사용자를 가져올 때 가드에서 추가된 request user를 통해 가져오도록 개선

--- src/common/decorator/current-user.decorator.ts
+++ src/common/decorator/current-user.decorator.ts
@@ -1,7 +1,5 @@
 import { ExecutionContext, createParamDecorator } from '@nestjs/common';
 
-import { decode } from 'jsonwebtoken';
-
 export interface ICurrentUser {
   id: string;
 }
@@ -10,12 +8,10 @@ export const CurrentUser = createParamDecorator(
   (_, context: ExecutionContext) => {
     const request = context.switchToHttp().getRequest();
 
-    const pureToken = request.headers.authorization.replace(/^Bearer\s/, '');
-
-    const jwtClaims = decode(pureToken, { json: true });
+    const { user } = request;
 
     return {
-      id: jwtClaims?.sub,
+      id: user?.id,
     };
   },
 );
