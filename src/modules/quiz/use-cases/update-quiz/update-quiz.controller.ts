a045306209d3d9e9f6a4dcdd1fb99fddbea74a8e
LeeSeokHo <rrgks@naver.com>
2025-11-03 16:44:14 +0900

    feat: 퀴즈 수정 시 imageUrl이 아닌 fileName을 받도록 변경

--- src/modules/quiz/use-cases/update-quiz/update-quiz.controller.ts
+++ src/modules/quiz/use-cases/update-quiz/update-quiz.controller.ts
@@ -32,8 +32,6 @@ import {
 import { ApiErrorResponse } from '@common/decorator/api-fail-response.decorator';
 import { AdminGuard } from '@common/guards/admin.guard';
 
-import { AssetUrlManager } from '@shared/asset/asset-url.manager';
-
 @ApiTags('quiz')
 @Controller()
 export class UpdateQuizController {
@@ -55,23 +53,12 @@ export class UpdateQuizController {
     @Body() body: UpdateQuizDto,
   ): Promise<QuizDto> {
     try {
-      if (
-        body.imageUrl !== null &&
-        body.imageUrl !== undefined &&
-        !AssetUrlManager.isValidUrl(body.imageUrl, 'quizImage')
-      ) {
-        throw new QuizImageNotFoundError();
-      }
-
       const command = new UpdateQuizCommand({
         quizId,
         type: body.type,
         answer: body.answer,
         question: body.question,
-        imageFileName: AssetUrlManager.urlToFileName(
-          body.imageUrl as string,
-          'quizImage',
-        ),
+        imageFileName: body.imageFileName,
       });
 
       const quiz = await this.commandBus.execute<UpdateQuizCommand, Quiz>(
