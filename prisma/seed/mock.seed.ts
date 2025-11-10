b73b455972a5ddef6b374655908c376990279228
LeeSeokHo <rrgks@naver.com>
2025-11-10 14:54:13 +0900

    chore: mock 시딩 스크립트 개선
    - fs 모듈이 아닌 json import를 하여 타입 세이프하게 개선
    - upsert 형식으로 변경
    - 클래스형으로 리펙토링
    - 삽입, 스킵 카운트 로깅

--- prisma/seed/mock.seed.ts
+++ prisma/seed/mock.seed.ts
@@ -1,43 +1,103 @@
+/* eslint-disable no-restricted-imports */
+import mockNicknameSources from './contents/nickname-source-mock.json';
+import mockQuizImages from './contents/quiz-image-mock.json';
+import mockQuizzes from './contents/quiz-mock.json';
 import { PrismaClient } from '@prisma/client';
-import { readFileSync } from 'fs';
-import { TSID } from 'tsid-ts';
 
 const prisma = new PrismaClient();
 
-const mockQuizzes = JSON.parse(
-  readFileSync(`${__dirname}/contents/quiz-mock.json`, 'utf-8'),
-);
-const mockNicknameSources = JSON.parse(
-  readFileSync(`${__dirname}/contents/nickname-source-mock.json`, 'utf-8'),
-);
+class Seeder {
+  counter = {
+    quizImage: { insertCount: 0, skipCount: 0 },
+    quiz: { insertCount: 0, skipCount: 0 },
+    nicknameSource: { insertCount: 0, skipCount: 0 },
+  };
 
-async function main() {
-  await prisma.quiz.createMany({
-    data: mockQuizzes.map((quiz) => {
-      return {
-        id: TSID.create().number.toString(),
+  constructor(
+    readonly prismaClient: PrismaClient,
+    readonly contents: {
+      quizImages: typeof mockQuizImages;
+      quizzes: typeof mockQuizzes;
+      nicknameSources: typeof mockNicknameSources;
+    },
+  ) {}
+
+  async run() {
+    await this.seedQuizImages();
+    await this.seedQuizzes();
+    await this.seedNicknameSources();
+  }
+
+  private async seedQuizImages() {
+    const quizImageResult = await prisma.quizImage.createMany({
+      data: this.contents.quizImages.map((quizImage) => ({
+        id: BigInt(quizImage.id),
+        category: quizImage.category,
+        name: quizImage.name,
+        originalFileName: quizImage.originalFileName,
+        fileName: quizImage.quizImageFileName,
+        extension: quizImage.extension,
+        contentType: quizImage.contentType,
+        contentLength: String(quizImage.contentLength),
+        width: quizImage.width,
+        height: quizImage.height,
+      })),
+      skipDuplicates: true,
+    });
+    this.counter.quizImage.insertCount += quizImageResult.count;
+    this.counter.quizImage.skipCount +=
+      this.contents.quizImages.length - quizImageResult.count;
+  }
+
+  private async seedQuizzes() {
+    const quizResult = await prisma.quiz.createMany({
+      data: this.contents.quizzes.map((quiz) => ({
+        id: BigInt(quiz.id),
         type: quiz.type,
-        imageUrl: quiz.imageUrl,
+        imageFileName: quiz.imageFileName,
         question: quiz.question,
         answer: quiz.answer,
-      };
-    }),
-  });
-  await prisma.nicknameSource.createMany({
-    data: mockNicknameSources.map((nicknameSource) => {
-      return {
-        id: TSID.create().number.toString(),
-        name: nicknameSource,
+      })),
+      skipDuplicates: true,
+    });
+    this.counter.quiz.insertCount += quizResult.count;
+    this.counter.quiz.skipCount +=
+      this.contents.quizzes.length - quizResult.count;
+  }
+
+  private async seedNicknameSources() {
+    const nicknameSourceResult = await prisma.nicknameSource.createMany({
+      data: this.contents.nicknameSources.map((nicknameSource) => ({
+        id: BigInt(nicknameSource.id),
+        name: nicknameSource.name,
         sequence: 0,
-      };
-    }),
-  });
+      })),
+      skipDuplicates: true,
+    });
+    this.counter.nicknameSource.insertCount += nicknameSourceResult.count;
+    this.counter.nicknameSource.skipCount +=
+      this.contents.nicknameSources.length - nicknameSourceResult.count;
+  }
 }
 
-main()
+const seeder = new Seeder(prisma, {
+  quizImages: mockQuizImages,
+  quizzes: mockQuizzes,
+  nicknameSources: mockNicknameSources,
+});
+
+seeder
+  .run()
   .then(() => {
-    console.log(`${mockQuizzes.length}개의 Mock Quiz 생성 완료`);
-    console.log(`${mockNicknameSources.length}개의 Mock 닉네임 소스 생성 완료`);
+    console.log(
+      `퀴즈 이미지: ${seeder.counter.quizImage.insertCount}개 삽입, ${seeder.counter.quizImage.skipCount}개 스킵`,
+    );
+    console.log(
+      `퀴즈: ${seeder.counter.quiz.insertCount}개 삽입, ${seeder.counter.quiz.skipCount}개 스킵`,
+    );
+    console.log(
+      `닉네임 소스: ${seeder.counter.nicknameSource.insertCount}개 삽입, ${seeder.counter.nicknameSource.skipCount}개 스킵`,
+    );
   })
   .catch(async (e) => {
     console.error(e);
